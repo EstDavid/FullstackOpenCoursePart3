fda56beb297e4ad41434c3504b62138a024c5455
Vandenynas <stlo.subs@gmail.com>
2023-01-19 17:55:04 +0100

    Implemented morgan logger

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
 app.use(express.json())
+
+// Creating custom token 'data' which returns the request.data stringified objecto from
+// the 'processData' middleware function
+morgan.token('data', (request, response) => request.data)
+
+const processData = (request, response, next) => {
+    request.data = JSON.stringify(request.body)
+    next()
+}
+
+// Using middleware function 'processData', which stringifies the request.body
+app.use(processData)
+
+// Using a custom format function which replicates the output of 
+// the 'tiny' format, and adds request data with HTTP POST resquests
+app.use(morgan((tokens, request, response) => {
+    let customFormat = [
+        tokens.method(request, response),
+        tokens.url(request, response),
+        tokens.status(request, response),
+        tokens.res(request, response, 'content-length'), '-',
+        tokens['response-time'](request, response), 'ms'
+    ]
+
+    if (request.method === 'POST') {
+        customFormat = customFormat.concat(tokens.data(request, response))
+    }
+    return customFormat.join(' ')
+}))
 
 let persons = [
     {
@@ -60,32 +91,9 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const generateId = () => {
-    let idFound = false
-
-    const maxNumber = 10000
-
-    // This loop will become infinite if the number of persons 
-    // reaches the maxNumber of persons value
-    while(!idFound) {
-
-        const id = Math.floor(Math.random()*maxNumber)
-
-        console.log('trying', id)
-
-        const idExists = persons.find(person => person.id === id)
-
-        if(!idExists) {
-            idFound = true
-            return id
-        }
-    }
-}
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
-
-    console.log(body)
 
     if(!body.name) {
         return response.status(400).json({
@@ -117,6 +125,24 @@ app.post('/api/persons', (request, response) => {
 
     response.json(person)
 })
+
+const generateId = () => {
+    let idFound = false
+
+    const maxNumber = 10000
+
+    // This loop will become infinite if the number of persons 
+    // reaches the maxNumber of persons value
+    while(!idFound) {
+        const id = Math.floor(Math.random()*maxNumber)
+        const idExists = persons.find(person => person.id === id)
+
+        if(!idExists) {
+            idFound = true
+            return id
+        }
+    }
+}
 
 const PORT = 3001
 app.listen(PORT, () => {
